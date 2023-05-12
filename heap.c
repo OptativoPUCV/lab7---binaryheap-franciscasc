f96bb3c52b9b6bb54a5b30164b7df681db55a2b4
Replit user <>
2023-05-12 12:40:36 +0000

    new try

--- heap.c
+++ heap.c
@@ -20,7 +20,7 @@ typedef struct Heap{
 void* heap_top(Heap* pq){
   int h = 0;
   if(pq->heapArray[h].priority > pq->heapArray[h + 1].priority){
-    return pq->heapArray->data;
+    return pq->heapArray[h].p->data;
   }
     return NULL;
 }
@@ -39,7 +39,7 @@ void heap_pop(Heap* pq){
 Heap* createHeap(){
   Heap* M;
   M = (Heap *) malloc(sizeof(Heap));
-  M->heapArray = (heapElem *) malloc(sizeof(heapElem));
+  M->heapArray = (heapElem *) malloc(sizeof(heapElem) * 3);
   M->size = 0;
   M->capac = 3;
    return M;
