230aa17572d57675fcd59b494bb010479c607283
Replit user <>
2023-05-12 14:07:55 +0000

    new try

--- heap.c
+++ heap.c
@@ -73,7 +73,7 @@ void heap_pop(Heap* pq){
       aux = pq->heapArray[h];
       pq->heapArray[h] = pq->heapArray[2*h + 1];
       pq->heapArray[2*h + 1] = aux;
-      break;
+      //break;
     }
 
     //caso 3
