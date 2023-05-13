2157db4256ce8c80dc3416af3130a0f55f861689
Replit user <>
2023-05-13 04:08:12 +0000

    new try

--- heap.c
+++ heap.c
@@ -76,7 +76,7 @@ void heap_pop(Heap* pq){
       pq->heapArray[mayor] = aux;
       h = mayor;
     }
-    pq->heapArray[h] = pq->heapArray[(h - 1)/2];
+    
     
     if(pq->heapArray[menor].priority > h){
       break;
@@ -85,6 +85,7 @@ void heap_pop(Heap* pq){
       pq->heapArray[menor] = aux;
       h = menor;*/
     }
+    pq->heapArray[h] = pq->heapArray[(h - 1)/2];
     
     if(pq->heapArray[(2*h + 1)].priority > pq->size -1 && pq->heapArray[(2*h + 2)].priority > pq->size - 1) 
       break;
