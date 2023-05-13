beedb6af78b1e1820e2ab7c108c6484e395143cc
Replit user <>
2023-05-13 03:21:05 +0000

    new try

--- heap.c
+++ heap.c
@@ -100,7 +100,7 @@ void heap_pop(Heap* pq){
       h = (2*h) + 1;
       //break;
     }*/
-     pq->heapArray[h] = pq->heapArray[(h - 1)/2];
+     pq->heapArray[h] = pq->heapArray[mayor];
     if(pq->heapArray[(2*h + 1)].priority > pq->size - 1 && pq->heapArray[(2*h + 2)].priority > pq->size - 1) 
       break;
 
