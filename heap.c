bf2688701f0a0767d84b8d8996dfa8969f276aaa
Replit user <>
2023-05-13 03:51:54 +0000

    new try

--- heap.c
+++ heap.c
@@ -57,7 +57,6 @@ void heap_pop(Heap* pq){
   heapElem aux =  pq->heapArray[h];
   pq->heapArray[h].priority = 0; 
   pq->heapArray[h] = pq->heapArray[(pq->size) - 1];
-  pq->heapArray[(pq->size) - 1] = aux;
   
   while(h < pq->size - 1){    
     //ver cuál hijo es mayor 
@@ -89,6 +88,7 @@ void heap_pop(Heap* pq){
     if(pq->heapArray[(2*h + 1)].priority > pq->size -1 && pq->heapArray[(2*h + 2)].priority > pq->size - 1) 
       break;
   }
+  pq->heapArray[(pq->size) - 1] = aux;
   pq->size--;
 }
 
