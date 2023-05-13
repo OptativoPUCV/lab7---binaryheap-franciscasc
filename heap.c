5719aceafb6238b7ba74f6cbda905236d5fc42b1
Replit user <>
2023-05-13 03:44:48 +0000

    new try

--- heap.c
+++ heap.c
@@ -85,7 +85,7 @@ void heap_pop(Heap* pq){
       h = menor;
     }
 
-    if((h - 1)/2 > h){
+    if((h - 1)/2 != h){
       aux = pq->heapArray[(h - 1)/2];
       pq->heapArray[(h - 1)/2] = pq->heapArray[h];
       pq->heapArray[h] = aux;
